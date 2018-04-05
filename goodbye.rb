26e5c47f65168abd015850c9a9ce92dc11c7f9d1
Katherine Ahn <katherine@weebly.com>
2018-04-05 00:46:46 +0000

    if i had to eat japanese

--- goodbye.rb
+++ goodbye.rb
@@ -3,3 +3,7 @@ puts "goodbye world!"
 dinner=["korean","chinese","italian","french"]
 
 puts "I want to eat #{dinner.sample} for dinner."
+
+japanese=["sushi","teriyaki","sukiyaki","seaweed"]
+
+puts "But if I had to eat Japanese, I would eat #{japanese.sample}."
